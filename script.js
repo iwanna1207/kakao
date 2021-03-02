b88d6816c5f3184656b5abcc32715f646ab11c66
iwanna1207 <sic6118@naver.com>
2021-03-02 16:23:09 +0900

    ver01

---
+++ script.js
@@ -0,0 +1,50 @@
+
+var page=1;
+getList();
+$("#txtQuery").on("keydown",function(e){
+    if(e.keyCode==13){
+        page=1; getList();
+    }
+});
+$("#btnSearch").on("click",function(){
+    page=1; getList();
+});
+$("#selSize").on("change",function(){
+    page=1; getList();
+});
+$("#btnPre").on("click",function(){
+    page--; getList();
+});
+$("#btnNext").on("click",function(){
+    page++; getList();
+});
+function getList(){
+    var query=$("#txtQuery").val();
+    var size=$("#selSize").val();
+    $.ajax({
+        type:"get",
+        url:url,
+        headers:{"Authorization" : "KakaoAK 86bcfbe2e730c6fd61219287087b0991"},
+        data:{"query":query, "size":size, "page":page},
+        dataType:"json",
+        success:function(data){
+            var temp=Handlebars.compile($("#temp").html());
+            $("#tbl").html(temp(data));
+
+            var lastPage=Math.ceil(data.meta.pageable_count/size);
+            $("#spanPage").html(page+" / "+lastPage);
+            $("#pageNum").html("검색결과 : "+lastPage+"페이지");
+
+            if(page==1){
+                $("#btnPre").attr("disabled",true);
+            }else{
+                $("#btnPre").attr("disabled",false);
+            }
+            if(page==lastPage){
+                $("#btnNext").attr("disabled",true);
+            }else{
+                $("#btnNext").attr("disabled",false);
+            }
+        }
+    });
+}
